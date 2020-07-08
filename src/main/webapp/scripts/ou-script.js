4e574caf43d63aba3347bf639005a72f9d324b87
Prashant <malyalaprashant@gmail.com>
2020-07-08 18:45:40 +0000

    Updates, deletes, and creates OUs given field properties and paths.

--- src/main/webapp/scripts/ou-script.js
+++ src/main/webapp/scripts/ou-script.js
@@ -370,7 +370,7 @@ function deleteOU() {
     headers: {
         'authorization': `Bearer ` + token,
     },
-    method: 'delete'
+    method: 'DELETE'
     })
     .then(response => {
         // refresh the page (getAllOUs call alone doesn't work, as puts new visual directly above old)
@@ -385,21 +385,16 @@ function deleteOU() {
  * Adds a new OU given a name, the parent's path, and (optionally) description / inheritance setting.
 */
 function createOU() {
-    var parentPath = '/' + document.getElementById('create-path').value;
-    var name = document.getElementById('create-name').value;
-    var descript =  document.getElementById('create-descript').value;
-    var blockInherit = document.getElementById('create-inherit').value;
+    var parentPath = '/' + document.getElementById('create-path').value.trim();
+    var name = document.getElementById('create-name').value.trim();
+    var descript =  document.getElementById('create-descript').value.trim();
+    var blockInherit = document.getElementById('create-inherit').value.trim();
 
     if (blockInherit == 'block') {
         blockInherit = true;
     } else {
         blockInherit = false;
     }
-
-    console.log(parentPath);
-    console.log(name);
-    console.log(descript);
-    console.log(blockInherit);
 
     var newOU = {
             "name": name,
@@ -408,25 +403,69 @@ function createOU() {
             "blockInheritance": blockInherit
         };
 
-    console.log(newOU);
-
     fetch(('https://www.googleapis.com/admin/directory/v1/customer/my_customer/orgunits'), {
     headers: {
         'authorization': `Bearer ` + token,
+        'dataType': 'application/json',
+        'Content-Type': 'application/json'
     },
     method: 'POST',
     body: JSON.stringify(newOU)
     })
     .then(response => {
         // refresh the page (getAllOUs call alone doesn't work, as puts new visual directly above old)
-        //location.reload();
-        console.log('hi there');
+        location.reload();
     })
     .catch((error) => {
         console.error('Error:', error);
     });
 }
 
-function updateOU(){
-    return;
+/*
+ * Updates an OU with a new name, parent path, description, or inheritance setting.
+*/
+function updateOU() {
+
+    var updateOU = {};
+
+    var ouPath = document.getElementById('update-path').value.trim();
+
+    var parentPath = document.getElementById('update-parent-path').value.trim();
+    var name = document.getElementById('update-name').value.trim();
+    var descript =  document.getElementById('update-descript').value.trim();
+    var blockInherit = document.getElementById('update-inherit').value.trim();
+
+    // update fields if non-empty
+    if (parentPath != '') {
+        updateOU.parentOrgUnitPath = '/' + parentPath;
+    }
+    if (name != '') {
+        updateOU.name = name;
+    }
+    if (descript != '') {
+        updateOU.description = descript;
+    }
+    if (blockInherit == 'block') {
+        updateOU.blockInheritance = true;
+    }
+    if (blockInherit == 'unblock') {
+        updateOU.blockInheritance = false;
+    }
+
+    fetch(('https://www.googleapis.com/admin/directory/v1/customer/my_customer/orgunits/' + ouPath), {
+    headers: {
+        'authorization': `Bearer ` + token,
+        'dataType': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    method: 'PUT',
+    body: JSON.stringify(updateOU)
+    })
+    .then(response => {
+        // refresh the page (getAllOUs call alone doesn't work, as puts new visual directly above old)
+        // location.reload();
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
 }
