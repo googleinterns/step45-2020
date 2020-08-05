c8b4045152bf6c4e2600dc9016863dc4768ac9a9
Prashant <malyalaprashant@gmail.com>
2020-08-05 17:36:34 +0000

    running into error with constructD3JSON test

--- src/test/javascript/test-ous.js
+++ src/test/javascript/test-ous.js
@@ -1,3 +1,25 @@
+describe("Test ComputeDepth", function() {
+
+  // Note that this function would never be called with the root OU, '/' (which assumes depth of 1)
+  var shortPathOU = {"orgUnitPath": "/layer1"};
+  var longPathOU = {"orgUnitPath": "/depth/six/path/shown/here"};
+
+  it("computing depth by path", function(done) {
+    
+    $.getScript('/src/main/webapp/scripts/ou-utils.js', function() {
+
+        var shortPathDepth = computeDepth(shortPathOU);
+        expect(shortPathDepth).toBe(2);
+
+        var longPathDepth = computeDepth(longPathOU);
+        expect(longPathDepth).toBe(6);
+
+        done();
+     });
+  });
+});
+
+
 describe("Test OUDepthSort", function() {
     
   var shortPath = {"orgUnitPath": "/onedepth"};
@@ -7,12 +29,126 @@ describe("Test OUDepthSort", function() {
     var depthDifference = 0;
     
     $.getScript('/src/main/webapp/scripts/ou-utils.js', function() {
-        var depthDifference = ouDepthSort(longPath, shortPath)
+        var depthDifference = ouDepthSort(longPath, shortPath);
         expect(depthDifference).toBe(2); 
         done();
      });
   });
 });
+
+
+// FAILING TEST
+describe("Test ConstructD3JSON", function() {
+
+  var testOUs = [];
+
+  // add data under /NA/business
+  addOUJSONToList(testOUs, "sales", "/NA/business");
+  addOUJSONToList(testOUs, "marketing", "/NA/business");
+
+  // add data under /EMEA
+  addOUJSONToList(testOUs, "all", "/EMEA");
+
+  // add data under root
+  addOUJSONToList(testOUs, "NA", "/");
+  addOUJSONToList(testOUs, "EMEA", "/");
+
+  // add data under /NA
+  addOUJSONToList(testOUs, "business", "/NA");
+  addOUJSONToList(testOUs, "engineering", "/NA");
+  addOUJSONToList(testOUs, "essentials", "/NA");
+
+
+  it("constructing D3 tree-like JSON", function(done) {
+    
+    $.getScript('/src/main/webapp/scripts/ou-utils.js', function() {
+        var expectedJSONTree = {
+            "name": "root",
+            "parentOrgUnitPath": "",
+            "description": "The root organizational unit.",
+            "orgUnitPath": "/",
+            "blockInheritance": false,
+            "children": [
+                {
+                    "name": "NA",
+                    "parentOrgUnitPath": "/",
+                    "children": [
+                        {
+                            "name": "business",
+                            "parentOrgUnitPath": "/NA",
+                            "children": [
+                                {
+                                    "name": "sales",
+                                    "parentOrgUnitPath": "/NA/business",
+                                    "children": []
+                                },
+                                {
+                                    "name": "marketing",
+                                    "parentOrgUnitPath": "/NA/business",
+                                    "children": []
+                                }
+                            ]
+                        },
+                        {
+                            "name": "engineering",
+                            "parentOrgUnitPath": "/NA",
+                            "children": []
+                        },
+                        {
+                            "name": "essentials",
+                            "parentOrgUnitPath": "/NA",
+                            "children": []
+                        }
+                    ]
+                },
+                {
+                    "name": "EMEA",
+                    "parentOrgUnitPath": "/",
+                    "children": [
+                        {
+                            "name": "all",
+                            "parentOrgUnitPath": "/EMEA",
+                            "children": []
+                        }
+                    ]
+                }
+            ]
+        };
+
+        // ensure data is sorted before pass in
+        testOUs.sort(ouDepthSort);
+
+        var treeLikeJSON = constructD3JSON(testOUs);
+
+        // none of the following expects worked
+        // ERROR: constructing D3 tree-like JSON <<< FAILURE!
+        // * Uncaught TypeError: Cannot read property 'match' of undefined thrown
+
+        // expect(_.isEqual(treeLikeJSON, expectedJSONTree)).toEqual(true);
+        expect(treeLikeJSON).toContain(jasmine.objectContaining(
+            {
+                "name": "root",
+                "parentOrgUnitPath": "",
+                "description": "The root organizational unit.",
+                "orgUnitPath": "/",
+                "blockInheritance": false
+            }
+        ));
+        // expect(JSONAssert.assertEquals(expectedJSONTree, treeLikeJSON, false)).toEqual(true);
+
+        done();
+     });
+  });
+});
+
+function addOUJSONToList(listOfOUs, name, parentOrgUnitPath) {
+    var outputOU = {};
+    outputOU.name = name;
+    outputOU.parentOrgUnitPath = parentOrgUnitPath;
+    listOfOUs.push(outputOU);
+}
+
+
 
 
 describe("Test SearchOU", function() {
